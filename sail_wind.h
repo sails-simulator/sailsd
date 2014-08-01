a84a0baf0df4f83cbf091003503dba9edf90f0fb
Louis Taylor <kragniz@gmail.com>
2014-08-01 22:34:55 +0100

    Add include check to sail_wind.h

--- sail_wind.h
+++ sail_wind.h
@@ -1,3 +1,6 @@
+#ifndef SAIL_WIND_H_
+#define SAIL_WIND_H_
+
 typedef struct _wind {
     double speed, direction, psi_ap, a_ap;
 } Wind;
@@ -6,3 +9,5 @@ Wind* sail_wind_new();
 
 double sail_wind_get_speed(const Wind *wind);
 double sail_wind_get_direction(const Wind *wind);
+
+#endif
