45e7c1ff7b8767a3d6f8193dd77da6744b7e63bb
Louis Taylor <louis@kragniz.eu>
2016-03-05 21:31:07 +0000

    Add gpl headers to logging.*

--- logging.c
+++ logging.c
@@ -1,3 +1,21 @@
+/* This file is part of sailsd
+ *
+ * Copyright (C) 2014-2016 Louis Taylor <louis@kragniz.eu>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdarg.h>
 #include <time.h>
@@ -37,12 +55,12 @@ static void vlog_msg(const enum log_level level,
     printf("\n");
 }
 
-static void log_msg(const enum log_level level, const char *format, ...) {
+/* static void log_msg(const enum log_level level, const char *format, ...) {
     va_list arglist;
     va_start(arglist, format);
     vlog_msg(level, format, arglist);
     va_end(arglist);
-}
+} */
 
 void log_info(const char *format, ...) {
     va_list arglist;
